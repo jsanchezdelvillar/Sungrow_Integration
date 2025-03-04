133881abadb9a9e3d159849429475d4299a1423a
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-03-04 16:53:22 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration from configuration.yaml."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the integration from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload an integration entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
