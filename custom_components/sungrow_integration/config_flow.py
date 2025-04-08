138f186abbbb84532f451917b61d7e612466bf58
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-04-08 16:53:20 +0200

    Update config_flow.py

--- custom_components/sungrow_integration/config_flow.py
+++ custom_components/sungrow_integration/config_flow.py
@@ -1,80 +1,113 @@
-import logging
-import voluptuous as vol
-import yaml
-import os
+"""Config flow for SolarCloud integration."""
+
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers import aiohttp_client
-from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD, CONF_APPKEY, CONF_X_ACCESS_KEY, CONF_PUBLIC_KEY, CONF_PS_KEY, CONF_POINT_ID_LIST
+import voluptuous as vol
+import uuid
 
-_LOGGER = logging.getLogger(__name__)
+from .const import DOMAIN
+from . import secrets_handler
 
-def read_secrets():
-    secrets_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'secrets.yaml')
-    if os.path.exists(secrets_path):
-        with open(secrets_path, 'r') as file:
-            return yaml.safe_load(file)
-    return {}
 
-secrets = read_secrets()
+CONF_API_KEY = "api_key"
+CONF_SECRET_KEY = "secret_key"
+CONF_RSA_KEY = "rsa_key"
+CONF_PS_KEY = "ps_key"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_POINT_ID_LIST = "point_id_list"
 
-DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_USERNAME, default=secrets.get(CONF_USERNAME, '')): str,
-    vol.Required(CONF_PASSWORD, default=secrets.get(CONF_PASSWORD, '')): str,
-    vol.Required(CONF_APPKEY, default=secrets.get(CONF_APPKEY, '')): str,
-    vol.Required(CONF_X_ACCESS_KEY, default=secrets.get(CONF_X_ACCESS_KEY, '')): str,
-    vol.Required(CONF_PUBLIC_KEY, default=secrets.get(CONF_PUBLIC_KEY, '')): str,
-    vol.Required(CONF_PS_KEY, default=secrets.get(CONF_PS_KEY, '')): str,
-    vol.Required(CONF_POINT_ID_LIST, default=secrets.get(CONF_POINT_ID_LIST, [])): [str],
-    vol.Optional("sensor_names", default=secrets.get("sensor_names", {})): vol.Schema({str: str})
-})
 
-class CustomSolarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Custom Solar."""
+class SolarCloudConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for SolarCloud."""
 
     VERSION = 1
 
+    def __init__(self):
+        self.data = {}
+        self.point_id_list = []
+
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         errors = {}
-        
-        _LOGGER.debug("async_step_user called with input: %s", user_input)
 
         if user_input is not None:
-            try:
-                _LOGGER.debug("Creating entry with data: %s", user_input)
-                self.update_secrets(user_input)
-                return self.async_create_entry(title="Custom Solar", data=user_input)
-            except Exception as e:
-                _LOGGER.error("Error creating entry: %s", e)
-                errors["base"] = "cannot_create_entry"
-        
+            self.data.update(user_input)
+            return await self.async_step_points()
+
         return self.async_show_form(
             step_id="user",
-            data_schema=DATA_SCHEMA,
+            data_schema=vol.Schema({
+                vol.Required(CONF_API_KEY): str,
+                vol.Required(CONF_SECRET_KEY): str,
+                vol.Required(CONF_RSA_KEY): str,
+                vol.Required(CONF_PS_KEY): str,
+                vol.Required(CONF_USERNAME): str,
+                vol.Required(CONF_PASSWORD): str,
+            }),
             errors=errors,
         )
 
-    def update_secrets(self, user_input):
-        secrets = read_secrets()
-        secrets.update(user_input)
-        secrets_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'secrets.yaml')
-        with open(secrets_path, 'w') as file:
-            yaml.safe_dump(secrets, file)
+    async def async_step_points(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            if user_input.get("add_point"):
+                return await self.async_step_add_point()
+
+            self.data[CONF_POINT_ID_LIST] = self.point_id_list
+            return self.async_create_entry(title="SolarCloud", data={})
+
+        schema = vol.Schema({
+            vol.Optional("add_point"): bool,
+        })
+
+        return self.async_show_form(
+            step_id="points",
+            data_schema=schema,
+            description_placeholders={
+                "points": str(self.point_id_list)
+            },
+            errors=errors,
+        )
+
+    async def async_step_add_point(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            self.point_id_list.append((user_input["id"], user_input["name"]))
+            return await self.async_step_points()
+
+        return self.async_show_form(
+            step_id="add_point",
+            data_schema=vol.Schema({
+                vol.Required("id"): str,
+                vol.Required("name"): str,
+            }),
+            errors=errors,
+        )
+
+    async def async_create_entry(self, title, data):
+        entry = await super().async_create_entry(title=title, data=data)
+
+        # Guardar en secrets.yaml
+        keys = [
+            CONF_API_KEY,
+            CONF_SECRET_KEY,
+            CONF_RSA_KEY,
+            CONF_PS_KEY,
+            CONF_USERNAME,
+            CONF_PASSWORD,
+            CONF_POINT_ID_LIST,
+        ]
+        save_data = self.data.copy()
+        save_data[CONF_POINT_ID_LIST] = self.point_id_list
+
+        secrets_handler.save_secrets(self.hass, entry.entry_id, save_data)
+
+        return entry
 
     @staticmethod
     @callback
-    def async_get_options_flow(entry):
-        _LOGGER.debug("async_get_options_flow called for entry: %s", entry.data)
-        return OptionsFlowHandler(entry)
-
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options flow."""
-
-    def __init__(self, entry):
-        """Initialize options flow."""
-        self.entry = entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        return self.async_show_form(step_id="init", data_schema=vol.Schema({}))
+    def async_get_options_flow(config_entry):
+        from .options_flow import SolarCloudOptionsFlow
+        return SolarCloudOptionsFlow(config_entry)
